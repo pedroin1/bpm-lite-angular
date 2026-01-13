68946403f285c4e1a0c0a823392506f6f01d7f2d
Pedro Luiz <pedroluizpb45@outlook.com>
2026-01-13 11:58:25 -0300

    corrigindo input

--- src/app/components/input/input.ts
+++ src/app/components/input/input.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   imports: [],
   templateUrl: './input.html',
   styleUrl: './input.scss',
-  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Input {}
